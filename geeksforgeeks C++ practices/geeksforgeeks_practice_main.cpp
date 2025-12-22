64574e88b1c88f0ebfe90d9137b8a487660a2b14
chinna <bansuay88@gmail.com>
2025-12-22 15:46:36 +0700

    Update geeksforgeeks_practice_main.cpp

--- geeksforgeeks C++ practices/geeksforgeeks_practice_main.cpp
+++ geeksforgeeks C++ practices/geeksforgeeks_practice_main.cpp
@@ -1,1 +1,24 @@
-// for practicing mainly, workable codes here
+Q1
+
+#include <iostream>
+int main() {
+    std::cout << "Hello World";
+}
+
+___________________________________________________
+
+Q2
+
+#include <iostream>
+int main() {
+    int number;
+    std::cout << "Enter any Number: ";
+    std::cin >> number;
+    std::cout << "Entered Number: ";
+    std::cout << number;
+}
+
+______________________________________________________
+
+Q3
+
